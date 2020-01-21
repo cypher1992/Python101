42e128536cd3d96bae26c75f801831d8e7218d60
Raymond <raymond.yassin@fdmgroup.com>
2020-01-21 01:19:52 -0500

    Basics varibles and assignments

---
+++ com/project/src/Basics/IntroPython.py
@@ -0,0 +1,21 @@
+# First Comment
+"""
+MultiLine Comment
+"""
+
+# Variable
+value  = 2+2
+value2 = (2+1)/2
+value3 = (7/3)
+value4 = value5 = 5
+value6, value7 = 4, -10
+# -------------------
+
+
+print( 2+2)
+print(value)
+print(value2)
+print(value3)
+print(value4 + value5) # 5+5
+print(value6)
+print(value7)
