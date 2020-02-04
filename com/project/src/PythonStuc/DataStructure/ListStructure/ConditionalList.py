caced7f1037c29dab653d098681bbe4e1c6aa869
Raymond Yassin <ryassin92@gmail.com>
2020-02-03 23:26:42 -0500

    List Comprehension For Loop and Challenge

---
+++ com/project/src/PythonStuc/DataStructure/ListStructure/ConditionalList.py
@@ -0,0 +1,48 @@
+"""
+Using Conditionals for List Composition
+"""
+# For loop gets x and then get y compare if not equals: return append list
+# then increments y and check again after hiting the end of the list
+# increments to the next x
+
+list = [(x, y) for x in [1, 2, 3] for y in [3, 4, 1] if x != y]
+
+print(list)
+
+comb = []
+
+for x in [1, 2, 3]:
+    for y in [3, 4, 1]:
+        if x != y:
+            comb.append((x, y))
+
+print(comb)
+
+# Challenge:
+vec = [-4, -2, 0, 2, 4]
+list = []
+# Times 2  the list values
+for x in vec:
+    list.append(x * 2)
+print(list)
+
+newVec = [x * 2 for x in vec]
+
+print(newVec)
+
+# filter list for negatives
+
+filterList = []
+for x in vec:
+    if (x > -1):
+        filterList.append(x)
+
+print(filterList)
+
+filterVec = [x for x in vec if x > -1]
+print(filterVec)
+
+
+# create a function that double an integer
+def square(x):
+    x ** 2
