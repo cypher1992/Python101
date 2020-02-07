be66fcbc0d72904ea187271e6db122329404a31d
Raymond Yassin <ryassin92@gmail.com>
2020-02-06 21:20:01 -0500

    Add tuple with and without Parentheseses

--- com/project/src/PythonStuc/DataStructure/TupleStructure/TupleStruc.py
+++ com/project/src/PythonStuc/DataStructure/TupleStructure/TupleStruc.py
@@ -7,3 +7,12 @@ tuple2 = ('k','v'),(2,3)
 
 print("Tuple1", tuple1)
 print("Tuple2", tuple2)
+
+# tuple with and without parentheses
+
+tupleWith = (1,)
+tupleWithout = 1,
+
+print("Have the same length ",len(tupleWith), len(tupleWithout))
+
+
