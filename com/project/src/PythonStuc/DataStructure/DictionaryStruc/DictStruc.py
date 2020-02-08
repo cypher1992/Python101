9003e27e5468fe0efd444869c8c83ea5d36e51d3
Raymond Yassin <ryassin92@gmail.com>
2020-02-07 23:28:30 -0500

    Add Dictionary Collection

---
+++ com/project/src/PythonStuc/DataStructure/DictionaryStruc/DictStruc.py
@@ -0,0 +1,17 @@
+# Dictionary key and value paired = immutable set like tuples
+
+dic = {'BlackStone':'BX','Citi':'C','JP Morgan':'JPM','Bank of America':'BAC'}
+
+print(dic)
+# append Wells Fargo
+dic['Wells Fargo'] = 'WFC'
+print(dic)
+
+# del Blackstone
+del dic['BlackStone']
+print(dic)
+# keys and values 
+keys = dic.keys()
+values = dic.values()
+
+print(keys,values)
