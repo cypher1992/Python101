7f743990a8d76a3268a2206c87caa4aa80119789
Raymond Yassin <ryassin92@gmail.com>
2020-02-09 22:36:57 -0500

    Add Tuple conversion to Stock

--- com/project/src/PythonStuc/DataStructure/DictionaryStruc/DictStruc.py
+++ com/project/src/PythonStuc/DataStructure/DictionaryStruc/DictStruc.py
@@ -15,3 +15,19 @@ keys = dic.keys()
 values = dic.values()
 
 print(keys,values)
+
+# Creating Dictionary with Dict
+dicStock = dict(Blackstone = 'BX',Citi = 'C', JP_Morgan ='JPM', Bank_Of_America = 'BAC')
+print(dicStock)
+
+#Convert Tuple to Dict
+tup1 = ("BlackStone", "BX")
+tup2 = ("Citi", "C")
+tup3 = ("JP Morgan", "JPM")
+tup4 = ("Bank of America", "BAC")
+
+StockList = [tup1,tup2,tup3,tup4]
+dicStockTup = dict(StockList)
+
+print("Dic StockList Conversion: ", dicStockTup)
+print(dicStockTup['Bank of America'])
