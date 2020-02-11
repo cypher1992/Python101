e959717c9190d696107786db17d690ecab7ef778
Raymond Yassin <ryassin92@gmail.com>
2020-02-10 22:25:44 -0500

    Add Iteration for Sequence

--- com/project/src/PythonStuc/DataStructure/Technique/IterationOverCollections.py
+++ com/project/src/PythonStuc/DataStructure/Technique/IterationOverCollections.py
@@ -7,5 +7,13 @@ stocks = {"BAC":"Bank Of America", "WFC":"Wells Fargo Corp", "JPM":"JP Morgan"}
 for k,v in stocks.items():
     print(k," : ",v)
 
+# Looping over Sequence
+for i,v in enumerate(["Item1","Item2","Item3"]):
+     print(i," : ",v)
 
+stocks = ["BX","APPL","JPM"]
+corp = ["Blackstone","Apple","JP Morgan"]
+
+for s,c in zip(stocks, corp):
+    print("Stock: ", s , "\nCorp: ", c)
 
