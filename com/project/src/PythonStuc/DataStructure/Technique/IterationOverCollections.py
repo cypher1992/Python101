ac8701e1e1d861fd0d286e70e2eb7a31d247a26e
Raymond Yassin <ryassin92@gmail.com>
2020-02-10 22:09:53 -0500

    Add Dictionary Iteration

---
+++ com/project/src/PythonStuc/DataStructure/Technique/IterationOverCollections.py
@@ -0,0 +1,11 @@
+# Looping Strats over Collections
+
+# Looping over dictionaries
+
+stocks = {"BAC":"Bank Of America", "WFC":"Wells Fargo Corp", "JPM":"JP Morgan"}
+
+for k,v in stocks.items():
+    print(k," : ",v)
+
+
+
