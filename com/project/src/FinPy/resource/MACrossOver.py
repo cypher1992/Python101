440971ec764e720a04b81fda7d5d964367c6f388
Raymond Yassin <ryassin92@gmail.com>
2020-02-11 22:22:17 -0500

    Add Technology Sector Analysis

--- com/project/src/FinPy/resource/MACrossOver.py
+++ com/project/src/FinPy/resource/MACrossOver.py
@@ -12,9 +12,9 @@ We short Apple if SMA 50 is greater than SMA 15, which signals downward price mo
 #For the purpose of this article, we are comparing Apple to the North American technology sector benchmark.
 def initialize(context):
     set_benchmark(symbol('SPY'))
-    context.boe = sid(698)
     context.aapl= sid(24)
-    context.wlmt= sid(8229)
+    context.snow= sid(43127)
+    context.amd = sid(351)
     context.max_notional = 1000000
 
 #Run trading program daily, one hour after market open.
@@ -30,36 +30,38 @@ def ma_crossover_handling(context, data):
         bar_count=200,
         frequency='1d'
     )
-    
-    boeHist = data.history(
-        context.boe,
+
+    snowHist = data.history(
+        context.snow,
         fields= 'price',
         bar_count=200,
         frequency='1d'
     )
-    
-    wlmtHist = data.history(
-        context.wlmt,
+
+    amdHist = data.history(
+        context.amd,
         fields= 'price',
         bar_count=200,
         frequency='1d'
     )
-    
- #Generating log of average of last 200 daily price data points.
+
+
+ #Generating log of average of last 50 daily price data points.
     log.info(applHist.head())
     sma_200 = applHist.mean()
     sma_50 = applHist[-50:].mean()
     sma_7 = applHist[-7:].mean()
-    
-    log.info(boeHist.head())
-    bsma_200 = boeHist.mean()
-    bsma_50 = boeHist[-50:].mean()
-    bsma_7 = boeHist[-7:].mean()
-    
-    log.info(wlmtHist.head())
-    wsma_200 = wlmtHist.mean()
-    wsma_50 = wlmtHist[-50:].mean()
-    wsma_7 = wlmtHist[-7:].mean()
+
+    log.info(snowHist.head())
+    snsma_200 = snowHist.mean()
+    snsma_50 = snowHist[-50:].mean()
+    snsma_7 = snowHist[-7:].mean()
+
+    log.info(amdHist.head())
+    amdsma_200 = amdHist.mean()
+    amdsma_50 = amdHist[-50:].mean()
+    amdsma_7 = amdHist[-7:].mean()
+
 
 #Returns all open orders.
     open_orders = get_open_orders()
@@ -68,29 +70,29 @@ def ma_crossover_handling(context, data):
 #execute scheduled market order if SMA 15 is greater than SMA 50.
     if sma_50 > sma_200:
         if context.aapl not in open_orders:
-            order_target_percent(context.aapl, 0.33)
-    
-    if bsma_50 > bsma_200:
-        if context.boe not in open_orders:
-            order_target_percent(context.boe, 0.33)
-            
-    if wsma_50 > wsma_200:
-        if context.boe not in open_orders:
-            order_target_percent(context.wlmt, 0.33)        
+            order_target_percent(context.aapl, 0.50)
+
+    if snsma_50 > snsma_200:
+        if context.snow not in open_orders:
+            order_target_percent(context.snow, 0.25)
+
+    if amdsma_50 > amdsma_200:
+        if context.amd not in open_orders:
+            order_target_percent(context.amd, 0.25)
 
 #If no open orders, short sell 100% of portfolio of AAPL &
 #execute scheduled market order if SMA 50 is greater than SMA 15.
     elif sma_200 > sma_7:
         if context.aapl not in open_orders:
-            order_target_percent(context.aapl, -0.33)
-            
-    elif bsma_200 > bsma_7:
-        if context.boe not in open_orders:
-            order_target_percent(context.boe, -0.33)
-            
-    elif wsma_200 > wsma_7:
-        if context.boe not in open_orders:
-            order_target_percent(context.wlmt, -0.33)  
+            order_target_percent(context.aapl, -0.50)
+
+    elif snsma_200 > snsma_7:
+        if context.snow not in open_orders:
+            order_target_percent(context.snow, -0.25)
+
+    elif amdsma_200 > amdsma_7:
+        if context.amd not in open_orders:
+            order_target_percent(context.amd, -0.25)
 
 #Generates leverage chart.
     record(leverage = context.account.leverage)
