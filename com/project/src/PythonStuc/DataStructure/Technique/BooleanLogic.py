e346fd872baad554cdc0b538bbbfa92097a32c84
Raymond Yassin <ryassin92@gmail.com>
2020-02-12 21:53:04 -0500

    add Boolean Logic Class for Series

---
+++ com/project/src/PythonStuc/DataStructure/Technique/BooleanLogic.py
@@ -0,0 +1,23 @@
+# Logic Structure
+
+# in
+# check if the instance occur sequence
+
+for i in range(0,15,1):
+    print(i)
+
+list = [1,4,5,6]
+print("\n")
+
+for i in list:
+    print(i)
+
+# Is and Is Not
+a = 'B'
+b = 'A'
+# is => compares two objects and returns true if the same and false if not the same
+# false
+print(a is b)
+
+# is not => compare two objects and returns false if the not the same and true  if the same
+print(a is not b)
