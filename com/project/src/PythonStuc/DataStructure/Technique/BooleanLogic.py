8ec5a94fc995fe4cf13cbfd50a810a55b450b36a
Raymond Yassin <ryassin92@gmail.com>
2020-02-12 22:19:42 -0500

    Add Logic with And OR NOT

--- com/project/src/PythonStuc/DataStructure/Technique/BooleanLogic.py
+++ com/project/src/PythonStuc/DataStructure/Technique/BooleanLogic.py
@@ -21,3 +21,18 @@ print(a is b)
 
 # is not => compare two objects and returns false if the not the same and true  if the same
 print(a is not b)
+
+# Chaining logic comparison
+
+a,b,c = 1,3,3
+print(a>b and b==c)
+print(a>b==c)
+
+# and -> true and false => false
+print(True and False)
+# or -> true or false => true
+print(True or False)
+# not -> not (true and false) = true
+# not -> not(true or false) = false
+print(not(True and False))
+print(not(True or False))
