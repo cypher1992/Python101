735c56c1c0ea867e39abce92872afc418778dcf9
Raymond Yassin <ryassin92@gmail.com>
2020-02-13 23:02:39 -0500

    Nested If/else with functions

--- com/project/src/PythonStuc/ControlFlowTools/IfControl.py
+++ com/project/src/PythonStuc/ControlFlowTools/IfControl.py
@@ -9,5 +9,31 @@ elif(stockPick == "Blackstone"):
 else:
     print("NA")
 
-    
+#---------------------
+
+id = "NA"
+id2 = "131411111114"
+isin2 = "NA"
+isin = "132131132"
+list = [id,id2]
+
+def getIsin():
+    return isin
+def getIsin2():
+    return isin2
+
+listID = []
+for i in list:
+    if(i != "NA"):
+        value = i
+        listID.append(value)
+    else:
+        if(getIsin2() != "NA"):
+            value = getIsin()
+            listID.append(value)
+        else:
+            value = None
+            listID.append(value)
+
+print(listID)
 
