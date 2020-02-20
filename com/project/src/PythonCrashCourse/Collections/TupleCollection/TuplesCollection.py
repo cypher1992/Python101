eae63ccb5862b16f1b512183b10b74aa0c5693d0
Raymond Yassin <ryassin92@gmail.com>
2020-02-19 19:34:59 -0500

    Add tuple collection

---
+++ com/project/src/PythonCrashCourse/Collections/TupleCollection/TuplesCollection.py
@@ -0,0 +1,16 @@
+# Tuple Collections
+# tuples are immutables
+# behaves like list
+
+tup = tuple(range(0,10,1))
+print(tup)
+
+for i in tup:
+    print(i)
+
+tup = tuple(range(0,20,2))
+print(tup)
+
+# intialize a tuple with parenthese
+tup = (10,25)
+print(tup)
