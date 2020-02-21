336cb8dd57bfb6f3a2c23e7720fc830f83964a31
Raymond Yassin <ryassin92@gmail.com>
2020-02-20 23:02:10 -0500

    Control Structure with Logical Operator and Equality Comparison

--- com/project/src/PythonCrashCourse/ControlStructure/IfControl/IfStatement.py
+++ com/project/src/PythonCrashCourse/ControlStructure/IfControl/IfStatement.py
@@ -9,3 +9,27 @@ for stock in stocks:
         print("%s: Competitor Stock; COMPLIANCE APPROVAL REQUIRED" % stock)
     else:
         print("%s: APPROVED TRADEABLE" % stock)
+
+#Logical Operators
+# C stands for condition
+# And (C AND C) => both needs to be true to execute
+if(stocks[0] == "BX" and stocks[6] == "WFC"):
+    print("BUY")
+
+# OR(C OR C) => only 1 condition needs to be true to execute
+if(stocks[0] == "BX" or stocks[2] =="GS"):
+    print("BUY")
+
+# Equality comparison
+# == Equal C == C
+print(2==2)
+# > Greater than C>C
+print(2>1)
+# < Less than C<C
+print(1<2)
+# >= Greater or equal than C>=C
+print(2>=2)
+# <= Less or equal than C<=C
+print(2<=2)
+# != not equal C != C
+print(1!=2)
