a365b108a5c6a88400f16ee3cc3f50189e68ec07
Raymond Yassin <ryassin92@gmail.com>
2020-02-21 23:47:53 -0500

    Add Dictionary appending new values

--- com/project/src/PythonCrashCourse/Collections/DictionaryCollection/Dict.py
+++ com/project/src/PythonCrashCourse/Collections/DictionaryCollection/Dict.py
@@ -1,8 +1,11 @@
 #Dictionary - using dictionary to represent a model such an object
 # instantiated with {} braces
 
-
 # Defining BlackStone Stock
 bxStock = {"COMPANY_NAME":"BlackStone","INDUSTRY":"Finance","SUB-INDUSTRY":"Private Equity","PRICE":22.50}
 # Accessing Value in Model
 print("SUB-INDUSTRY: %s" % bxStock['SUB-INDUSTRY'])
+
+# Adding Values to dictionary
+bxStock["SANCTION"] = "NON-SANCTION"
+print(bxStock)
