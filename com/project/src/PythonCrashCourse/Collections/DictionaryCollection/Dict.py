a8385972c09023f50d28bb7244072b82f345fa37
Raymond Yassin <ryassin92@gmail.com>
2020-02-21 23:44:31 -0500

    Adding Dictionary stucture and definition

---
+++ com/project/src/PythonCrashCourse/Collections/DictionaryCollection/Dict.py
@@ -0,0 +1,8 @@
+#Dictionary - using dictionary to represent a model such an object
+# instantiated with {} braces
+
+
+# Defining BlackStone Stock
+bxStock = {"COMPANY_NAME":"BlackStone","INDUSTRY":"Finance","SUB-INDUSTRY":"Private Equity","PRICE":22.50}
+# Accessing Value in Model
+print("SUB-INDUSTRY: %s" % bxStock['SUB-INDUSTRY'])
