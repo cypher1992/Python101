d6b0116408a2b4316af2e81e4d99d93e0aa4fb0b
Raymond Yassin <ryassin92@gmail.com>
2020-02-22 23:29:36 -0500

    Adding Mod Value, Looping through dictionary, getting values and key, and nesting collections

--- com/project/src/PythonCrashCourse/Collections/DictionaryCollection/Dict.py
+++ com/project/src/PythonCrashCourse/Collections/DictionaryCollection/Dict.py
@@ -12,5 +12,48 @@ print(bxStock)
 
 #Emtpy Dictionary
 gsStock ={}
-
 print("Goldman Sacs Stocks: %s" % gsStock)
+
+# Modifying the Value
+bxStock["SUB-INDUSTRY"] = "FinTec"
+print(bxStock)
+
+#Looping through a dictionary:
+for key,value in bxStock.items():
+    print("Key: %s \nValue: %s" % (key,value))
+
+#Looping through keys in dictionary:
+for key in bxStock.keys():
+    print("Key: %s" % key)
+
+#Looping through keys in order of dictionary:
+for key in sorted(bxStock.keys()):
+    print("Key: %s" % key)
+
+#Looping through values in dictionary:
+for value in bxStock.values():
+    print("Value: %s" % value)
+
+# Nesting Collections
+# List nested into dictionary
+dailyPrice = [22.14,22.16,25.62,30.47,27.91]
+
+bxStock["DailyPrice"] = dailyPrice
+print(bxStock)
+
+# Tuple nested into dictionary
+hqAddress = ("345 Park Ave", "NY","NY","10154")
+bxStock["Address"] = hqAddress
+print(bxStock)
+
+#Dictionary in a Dictionary
+businesses = {"Hedgefund":"BAAM","Credit":"GSO","Private_Equity":"PE & Strats","Real_Estate":"Real_Estate"}
+bxStock["Business_Units"]=businesses
+print(bxStock)
+
+gsStock = {"COMPANY_NAME":"Goldman Sachs","INDUSTRY":"Finance","SUB-INDUSTRY":"Investment Banking","PRICE":120.50}
+stocks = [bxStock,gsStock]
+
+for stock in stocks:
+    for k,v in stock.items():
+        print("Key: %s \nValue: %s" % (k,v))
