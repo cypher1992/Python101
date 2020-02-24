a90d6d1f9dc074bda67cf4c950666970336c7fe6
Raymond Yassin <ryassin92@gmail.com>
2020-02-23 21:12:18 -0500

    PCC: While Iteration with break,continue,and flags

--- com/project/src/PythonCrashCourse/ControlStructure/WhileIteration/WhileLoop.py
+++ com/project/src/PythonCrashCourse/ControlStructure/WhileIteration/WhileLoop.py
@@ -8,4 +8,36 @@ while(counter < size):
     print(stock[counter])
     counter+= 1
 
+# using flag to exit condition
+
+boolean = True
+
+while boolean:
+    message = input("Enter Message: ")
+    if(message == "exit"):
+        boolean = False
+    else:
+        print(message)
+
+#using break to exit condition
+while True:
+    message = input("Enter Message: ")
+    if(message == "exit"):
+        break
+    else:
+        print(message)
+
+#using continue in a while loop
+counter = -1
+while(counter < size):
+    print(counter)
+    if(counter%2 ==0 ):
+        counter += 1
+        continue
+    elif(counter == size-1):
+        break
+    else:
+        counter += 1
+        print(stock[counter])
+
 
