20b3422cf96fc61fe7c5054518185f5eeeaff672
Raymond Yassin <ryassin92@gmail.com>
2020-02-23 20:40:03 -0500

    PCC: While Iteration

---
+++ com/project/src/PythonCrashCourse/ControlStructure/WhileIteration/WhileLoop.py
@@ -0,0 +1,11 @@
+# While loop runs as or while, a certain condition is satisfied
+
+stock = ["BX","JPM","C","BAC","WFC"]
+size = len(stock)
+counter = 0
+
+while(counter < size):
+    print(stock[counter])
+    counter+= 1
+
+
