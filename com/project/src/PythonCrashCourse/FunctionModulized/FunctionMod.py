2596762c9522d5a1166e68cc87ba55a25f36f8ce
Raymond Yassin <ryassin92@gmail.com>
2020-02-24 22:23:28 -0500

    PCC: basics on Function

---
+++ com/project/src/PythonCrashCourse/FunctionModulized/FunctionMod.py
@@ -0,0 +1,12 @@
+#Functions - blocks of code specified to do one job!
+# Write function
+# pass parameters
+# return a type
+# create module files for functions
+
+# simple function: starts with a def name():
+
+def hw():
+    print("Hello you SOB")
+
+hw()
