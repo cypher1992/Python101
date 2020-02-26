77ecdf449c472be758d785a36a0a40e640ce0935
Raymond Yassin <ryassin92@gmail.com>
2020-02-25 21:18:05 -0500

    FunctionMod: Add define func with parameter and return value

--- com/project/src/PythonCrashCourse/FunctionModulized/FunctionMod.py
+++ com/project/src/PythonCrashCourse/FunctionModulized/FunctionMod.py
@@ -5,8 +5,25 @@
 # create module files for functions
 
 # simple function: starts with a def name():
-
 def hw():
     print("Hello you SOB")
 
-hw()
+
+# passing parameters to a function
+def printAdd(num,num2):
+    print(str(num+num2))
+
+def add(num,num2):
+    return num+num2
+
+
+
+def main():
+    #call a function
+    hw()
+    printAdd(2,2)
+    print(str(add(2+2,2)))
+
+"""----------------------------------------------"""
+
+main()
