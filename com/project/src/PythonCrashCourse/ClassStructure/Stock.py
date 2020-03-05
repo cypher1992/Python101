245d936e117e03b6f833d50be6c0f70afe5e1c6e
Raymond Yassin <ryassin92@gmail.com>
2020-03-04 21:15:59 -0500

    Class Structure: Define stock with constructor

---
+++ com/project/src/PythonCrashCourse/ClassStructure/Stock.py
@@ -0,0 +1,20 @@
+#Stock Class
+#Class has both attributes and methods
+
+class Stock():
+    """Model Stock Class"""
+
+    def __init__(self,company,ticker,price,date):
+        """Intialize Stock with:
+            company
+            ticker
+            price
+            date
+        """
+        self.company = company
+        self.ticker = ticker
+        self.price = price
+        self.date = date
+
+    #behavior of a stock
+    
