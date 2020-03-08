b23ad8c2ed2f31bb278f483bb0b10d0374e30d2b
Raymond Yassin <ryassin92@gmail.com>
2020-03-07 21:29:00 -0500

    UnitTest: Add Stock Class and Tested test_getTicker_returns_BX()

--- com/project/src/PythonCrashCourse/UnitTesting/TestClass.py
+++ com/project/src/PythonCrashCourse/UnitTesting/TestClass.py
@@ -1,1 +1,27 @@
 # Uniting testing with unittest
+# unit test - verifies that specific part of function behavior works
+# test case - is a collection of unit test that together proves that a function behaves as suppoised
+# Good Test Case - considers all possibilities of input
+# full coverage - includes a full range of unit tests covering all possible ways to use function
+
+import unittest
+from com.project.src.PythonCrashCourse.ClassStructure.Stock import Stock
+
+
+class StockTest(unittest.TestCase):
+        """Test Case for Stock.py"""
+        # setup
+        def test_getTicker_returns_BX(self):
+            """bx.getTicker() expected 'BX'"""
+            bx = Stock("BlackStone", "BX", 36.44, "3/7/20")
+            tickerName = bx.getTicker()
+            expected = "BX"
+            self.assertEqual(tickerName,expected)
+
+
+
+def main():
+    if __name__ == '__main__':
+        unittest.main()
+
+main()
