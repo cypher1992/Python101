1bdcb75280090d311dee24c1ec08327e2f738359
Raymond Yassin <ryassin92@gmail.com>
2020-03-16 22:17:17 -0400

    IOFiles: refactor test_readDelimterByLine_returns_ListOfLines() to use new File

--- com/project/src/PythonCrashCourse/UnitTesting/TestIOFile.py
+++ com/project/src/PythonCrashCourse/UnitTesting/TestIOFile.py
@@ -17,20 +17,9 @@ class IOFileTest(unittest.TestCase):
     def test_readDelimterByLine_returns_LengthOf6(self, path=path):
         """io.readDelimterByLine() expected 'ListOfLine'"""
         iof = IOFile
-        path += 'delimit.txt'
-        for i in range(0, 5):
-            iof.writeToFileDelimiter(iof, path, 'Goldman Sacs', 'GS', ',')
+        path += 'delimitRead.txt'
         actual = iof.readDelimterByLine(iof, path, ',')
         actual = len(actual)
         expected = 5
-        self.assertEqual(actual, expected)
-
-    def test_writeToFileDelimter_returns_LengthOf1(self, path=path):
-        iof = IOFile
-        path += 'delimit.txt'
-        iof.writeToFileDelimiter(iof, path, 'Goldman Sacs', 'GS', ',')
-        actual = iof.readDelimterByLine(iof, path, ',')
-        actual = len(actual)
-        expected = 1
         self.assertEqual(actual, expected)
 
