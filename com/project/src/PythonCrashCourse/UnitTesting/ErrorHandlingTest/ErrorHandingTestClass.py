9d048ff206cb6d727467c1c065d6e1ed92f4d487
Raymond Yassin <ryassin92@gmail.com>
2020-03-21 21:48:27 -0400

    ErrorHandling Test:  Add setList() and getList() test

--- com/project/src/PythonCrashCourse/UnitTesting/ErrorHandlingTest/ErrorHandingTestClass.py
+++ com/project/src/PythonCrashCourse/UnitTesting/ErrorHandlingTest/ErrorHandingTestClass.py
@@ -3,4 +3,19 @@ from com.project.src.PythonCrashCourse.ErrorHandling.TryAndCatchErrors import Li
 
 class TryAndCatchTest(unittest.TestCase):
 
-    pass
+    def test_getList_returns_List(self):
+        lst = [1,2,3,4]
+        tace = ListClass(lst)
+        actual = tace.getList()
+        expected = [1,2,3,4]
+        self.assertEqual(actual,expected)
+
+    def test_setList_returns_newList(self):
+        lst = [1,2,3,4]
+        newList = [4,3,2,1]
+        tace = ListClass(lst)
+        tace.setList(newList)
+        actual = tace.getList()
+        expected = [4,3,2,1]
+
+        self.assertEqual(actual,expected)
