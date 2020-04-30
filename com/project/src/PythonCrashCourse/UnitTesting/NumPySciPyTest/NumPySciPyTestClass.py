f4dc85d5a5132fc6468aa371614cc4270b6afe65
Raymond Yassin <ryassin92@gmail.com>
2020-04-29 21:36:18 -0400

    Python101 NumpySciPy: test func numpyArray(emptyArray) if all the value is equivalent

--- com/project/src/PythonCrashCourse/UnitTesting/NumPySciPyTest/NumPySciPyTestClass.py
+++ com/project/src/PythonCrashCourse/UnitTesting/NumPySciPyTest/NumPySciPyTestClass.py
@@ -6,6 +6,19 @@ from com.project.src.PythonCrashCourse.DataScience.NumPySciPy import NumpySciPy
 
 class NumpySciPyTestClass(unittest.TestCase):
 
+    def test_emptyArrayNumpyArray_returns_array(self):
+        npsp = NumpySciPy()
+        arrays = []
+        actual = npsp.numpyArray(arrays)
+        expected = np.array(arrays)
+        boolArray= actual == expected
+        isTrue =None
+        if False in boolArray:
+            isTrue = False
+        else:
+            isTrue = True
+        self.assertTrue(isTrue)
+
     def test_numpyArray_returns_array(self):
         npsp = NumpySciPy()
         arrays = [[1,2,3],[3,4,6]]
