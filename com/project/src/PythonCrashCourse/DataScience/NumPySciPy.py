c6d099761c1db79ac5d1f9e992447ff5f8a54c90
Raymond Yassin <ryassin92@gmail.com>
2020-05-18 22:33:44 -0400

    NumpySciPy Pandas: refactor valueCountDF(dataframe) to countDF() returns count foreach of columns

--- com/project/src/PythonCrashCourse/DataScience/NumPySciPy.py
+++ com/project/src/PythonCrashCourse/DataScience/NumPySciPy.py
@@ -53,5 +53,5 @@ class NumpySciPy:
     def averageDF(self,dataframe):
         return dataframe.mean()
 
-    def valueCountDF(self,dataframe):
-        return dataframe.value_counts()
+    def countDF(self,dataframe):
+        return dataframe.count()
