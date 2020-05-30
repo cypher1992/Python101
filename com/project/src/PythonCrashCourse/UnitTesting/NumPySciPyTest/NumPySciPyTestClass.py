362c1aa8cba5beb81836cc99ab6b6e47fb78f444
Raymond Yassin <ryassin92@gmail.com>
2020-05-30 02:37:12 -0400

    Python101 NumpySciPy: test greaterThanEqualsFilterColumnDF(df,Company,Blackstone)

--- com/project/src/PythonCrashCourse/UnitTesting/NumPySciPyTest/NumPySciPyTestClass.py
+++ com/project/src/PythonCrashCourse/UnitTesting/NumPySciPyTest/NumPySciPyTestClass.py
@@ -517,4 +517,16 @@ class NumpySciPyTestClass(unittest.TestCase):
         df = npsy.initDataFrame(stockData)
         actual = npsy.filterColumnDFAdv(df, "Company", "BlackStone","NOT_EQUALS")
 
+    def testGreaterThanEqualsColumnDFAdvForCompanyBlackstone(self):
+        stockData = {
+            'Company': ["BlackStone", "KKR", "Chase", "Bank Of America", "Wells Fargo", "Morgan Stanley"],
+            "Closing Price": [56.26, 21.60, 100.21, 26.75, 84.61, 246.25],
+            "LocationHQ": ["New York City,NY", "New York City,NY", "New York City,NY", "Charlotte, NC",
+                           "San Francisco, CA", "New York City,NY"]
+        }
+        npsy = NumpySciPy()
+        df = npsy.initDataFrame(stockData)
+        actual = npsy.greaterThanFilterColumnDF(df, "Company", "BlackStone")
+        print(actual)
+
 
