dfa8ecb5cf9c1365c48b925f86d93f4612e3366d
Raymond Yassin <ryassin92@gmail.com>
2020-06-12 21:57:34 -0400

    DataStructure: List() add getList returns self.List()

--- com/project/src/PythonCrashCourse/DataStructure/ListStructure.py
+++ com/project/src/PythonCrashCourse/DataStructure/ListStructure.py
@@ -1,7 +1,10 @@
 class List():
-
+    
     def __init(self,list):
         self.List = list
+
+    def getList(self):
+        return self.List
 
     def appendToList(self,value):
         return list.append(value)
