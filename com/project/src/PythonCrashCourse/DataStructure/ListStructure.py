150f5cf289ae75d5b4f628bed6b72a1c6a45d471
Raymond Yassin <ryassin92@gmail.com>
2020-06-12 21:58:44 -0400

    DataStructure: List() add setList(newList) returns self.newList()

--- com/project/src/PythonCrashCourse/DataStructure/ListStructure.py
+++ com/project/src/PythonCrashCourse/DataStructure/ListStructure.py
@@ -1,10 +1,13 @@
 class List():
-    
+
     def __init(self,list):
         self.List = list
 
     def getList(self):
         return self.List
+
+    def setList(self,newList):
+        self.List = newList
 
     def appendToList(self,value):
         return list.append(value)
