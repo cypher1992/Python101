469496bc2241be0bfd96fd44d677f7a06791a135
Raymond Yassin <ryassin92@gmail.com>
2020-06-19 23:30:12 -0400

    DataStructure GatherData: add getDataSource2()

--- com/project/src/Experimentation/GatherDataWithThreads/GatherData.py
+++ com/project/src/Experimentation/GatherDataWithThreads/GatherData.py
@@ -5,3 +5,11 @@ class GatherData():
         for i in 1000000:
             list.append(i)
         return list
+
+    def getDataSource2(self):
+        list = []
+        for i in range(100, 1000, 2):
+            list.append(i)
+        return list
+
+    
