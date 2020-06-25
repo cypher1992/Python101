a42c60c661368171737137bdf8b3b415dd78c084
Raymond Yassin <ryassin92@gmail.com>
2020-06-24 21:57:39 -0400

    DataStructure ThreadingData: add test_implementDurationOfThreadIsLessThan2sec()

--- com/project/src/PythonCrashCourse/UnitTesting/GatherDataTest/ThreadingDataTest.py
+++ com/project/src/PythonCrashCourse/UnitTesting/GatherDataTest/ThreadingDataTest.py
@@ -17,6 +17,7 @@ class ThreadingDataTest(unittest.TestCase):
         start = time.perf_counter()
         gd = GatherData()
         td = ThreadingData()
+        # when it comes to using the function in thread. No Parenthesis
         thread = td.createThread(function=gd.getDataSource1)
         thread2 = td.createThread(function=gd.getDataSource3)
         td.startThread(thread)
@@ -28,5 +29,19 @@ class ThreadingDataTest(unittest.TestCase):
         td.joinThread(thread2)
         finish = time.perf_counter()
         self.assertTrue(finish-start < 3)
+
+    def test_implementDuractionOfThreadsIsLessThan2Sec(self):
+        que = queue.Queue()
+        start = time.perf_counter()
+        gd = GatherData()
+        t1 = threading.Thread(target = que.put(gd.getDataSource1))
+        t2 = threading.Thread(target = que.put(gd.getDataSource1))
+        t1.start()
+        t2.start()
+        t1.join()
+        t2.join()
+        finish = time.perf_counter()
+        print(que.qsize())
+        self.assertTrue(finish - start < 2)
 
 
