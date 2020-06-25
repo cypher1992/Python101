d5d119a6246c582df184b4995d4721172a6de8cd
Raymond Yassin <ryassin92@gmail.com>
2020-06-24 21:53:05 -0400

    DataStructure ThreadingData: add test_durationOfThreadsIslessThan3Secs

--- com/project/src/PythonCrashCourse/UnitTesting/GatherDataTest/ThreadingDataTest.py
+++ com/project/src/PythonCrashCourse/UnitTesting/GatherDataTest/ThreadingDataTest.py
@@ -12,3 +12,21 @@ class ThreadingDataTest(unittest.TestCase):
         gd.getDataSource3()
         finish = time.perf_counter()
         self.assertTrue(finish-start>3)
+
+    def test_duractionOfThreadsIsLessThan3Sec(self):
+        start = time.perf_counter()
+        gd = GatherData()
+        td = ThreadingData()
+        thread = td.createThread(function=gd.getDataSource1)
+        thread2 = td.createThread(function=gd.getDataSource3)
+        td.startThread(thread)
+        td.startThread(thread2)
+        threadList = []
+        threadList.append(thread)
+        threadList.append(thread2)
+        td.joinThread(thread)
+        td.joinThread(thread2)
+        finish = time.perf_counter()
+        self.assertTrue(finish-start < 3)
+
+
