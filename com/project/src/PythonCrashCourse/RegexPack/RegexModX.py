a138046bc902c0a63648c0f01e40a87d645c5a6c
Raymond Yassin <ryassin92@gmail.com>
2020-07-05 16:50:05 -0400

    RegexX: add findLetters() return boolean

--- com/project/src/PythonCrashCourse/RegexPack/RegexModX.py
+++ com/project/src/PythonCrashCourse/RegexPack/RegexModX.py
@@ -1,11 +1,18 @@
+import re
 class Regex():
 
     def __init__(self,string):
         self.string = string
-
 
     def getString(self):
         return self.string
 
     def setString(self,string):
         self.string = string
+
+    def findDigits(self):
+        pattern = "[a-ZA-Z]"
+        if(re.search(pattern,self.getString())):
+            True
+        else:
+            False
