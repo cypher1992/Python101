5dc0aabb28240c15b0bd60f6c002566b7b324e46
Raymond Yassin <ryassin92@gmail.com>
2020-07-30 21:20:59 -0400

    PYTHON101 FlaskApp: add anotherPage() that points to another page and renders that page html

--- com/project/src/FlaskApp/app.py
+++ com/project/src/FlaskApp/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return '<h1>Hello!</h1>'
+
+@app.route('/AnotherPage')
+def anotherPage():
+    return render_template('anotherPage.html')
 
 if __name__ == "__main__":
     app.run()
